6ffd9dba4ea9500f7e894c23745d3e072d91e6f8
Tim Flood <tflood9@gmail.com>
2021-03-21 14:54:12 -0400

    commit tree ef694017bc538df01a7b559db4b52b793ecc51a3

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+
+  decrement = () => {
+    const newCount = this.state.secondsLeft - 1;
+    this.setState({
+      secondsLeft: newCount,
+    });
+  };
+  render() {
+    if (this.state.secondsLeft !== 0)
+      return <div>{this.state.secondsLeft} seconds left before I go boom!</div>;
+    else return <div>Boom!</div>;
+  }
+}
